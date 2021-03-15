80cc62060b761748a69d46ec995233e6987b67dd
yizhou0201 <52442482+yizhou0201@users.noreply.github.com>
2021-03-15 16:36:05 -0400

    Update master.sh
    Add use-conda for environment

--- master.sh
+++ master.sh
@@ -10,8 +10,8 @@ then
     cd single_end_pip
     wget https://ftp.ncbi.nlm.nih.gov/blast/db/ref_viruses_rep_genomes.tar.gz
     tar -xzvf ref_viruses_rep_genomes.tar.gz
-    snakemake --cores $core ../$sample/star_unmapped/align_unmapped.fq ../$sample/fastqc_report//single_end_trimmed_fastqc.html ../$sample/assembled_contigs
-    snakemake --cores $core ../$sample/blast_result/blast_out.txt ../$sample/ORFfinder ../$sample/RNAfold_output/secondary_structure.str
+    snakemake --cores $core ../$sample/star_unmapped/align_unmapped.fq ../$sample/fastqc_report//single_end_trimmed_fastqc.html ../$sample/assembled_contigs --use-conda
+    snakemake --cores $core ../$sample/blast_result/blast_out.txt ../$sample/ORFfinder ../$sample/RNAfold_output/secondary_structure.str --use-conda
 
 elif [ $mode=="PE" ];
 then
@@ -20,6 +20,6 @@ then
     wget https://ftp.ncbi.nlm.nih.gov/blast/db/ref_viruses_rep_genomes.tar.gz
     tar -xzvf ref_viruses_rep_genomes.tar.gz
     snakemake --cores $core ../$sample/star_unmapped/align_unmapped.fq ../$sample/fastqc_report/r1_paired_trimmed_fastqc.html \
-    ../$sample/fastqc_report/r2_paired_trimmed_fastqc.html ../$sample/assembled_contigs
-    snakemake --cores $core ../$sample/blast_result/blast_out.txt ../$sample/ORFfinder ../$sample/RNAfold_output/secondary_structure.str
+    ../$sample/fastqc_report/r2_paired_trimmed_fastqc.html ../$sample/assembled_contigs --use-conda
+    snakemake --cores $core ../$sample/blast_result/blast_out.txt ../$sample/ORFfinder ../$sample/RNAfold_output/secondary_structure.str --use-conda
 fi
